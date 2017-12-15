c7e066e3e7a4b7519da9bf41bb8e3fc16dfa33f5
Josh <Josh@WIN-PTG2CMLV3NR>
2017-12-14 23:06:51 -0500

    t1 added constructer

--- SmiteCalc/src/smitecalc/TierOne.java
+++ SmiteCalc/src/smitecalc/TierOne.java
@@ -7,4 +7,11 @@ package smitecalc;
  */
 public class TierOne extends Item {
     
+    public TierOne(String name, String powType, String passive, int cost){
+        this.name = name;
+        this. powType = powType;
+        this.passive = passive;
+        this.cost = cost;
+        
+    }
 }
