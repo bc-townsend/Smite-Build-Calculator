b27a9c19e2e9749c2ca8f2d85e0b3a0c9b705cfd
Josh <Josh@WIN-PTG2CMLV3NR>
2017-12-14 22:31:11 -0500

    Gorilla bears

--- SmiteCalc/src/smitecalc/SmiteCalc.java
+++ SmiteCalc/src/smitecalc/SmiteCalc.java
@@ -16,7 +16,7 @@ public class SmiteCalc {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello Asgard");
+        System.out.println("Hello Asgard and the japs");
     }
     
 }
